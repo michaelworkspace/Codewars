40b0e4f59b2d9ce2bf51861bd8ae9d39e74de688
Michael Le <admin@michaelworkspace.com>
2020-03-02 17:27:15 -0600

    Adding A Needle in a Haystack

---
+++ A_Needle_In_A_Haystack.py
@@ -0,0 +1,16 @@
+"""
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+find_needle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+should return "found the needle at position 5"
+
+"""
+
+def find_needle(haystack):
+    return ''.join(f'found the needle at position {i}' for i, n in enumerate(haystack) if n == 'needle')
