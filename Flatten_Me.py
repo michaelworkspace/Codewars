593ba506e03467752b2ef7419a8541542313a03a
Michael Le <admin@michaelworkspace.com>
2020-03-02 22:26:59 -0600

    Flatten Me

---
+++ Flatten_Me.py
@@ -0,0 +1,32 @@
+"""
+In this kata, your task is to create a function that takes a single list as an argument and returns a flattened list. The input list will have a maximum of one level of nesting (list(s) inside of a list).
+
+# no nesting
+[1, 2, 3]
+
+# one level of nesting
+[1, [2, 3]]
+Examples
+>>> flatten_me(['!', '?'])
+['!', '?']
+
+>>> flatten_me([1, [2, 3], 4])
+[1, 2, 3, 4]
+
+>>> flatten_me([['a', 'b'], 'c', ['d']])
+['a', 'b', 'c', 'd']
+
+>>> flatten_me([[True, False], ['!'], ['?'], [71, '@']]) 
+[True, False, '!', '?', 71, '@']
+
+"""
+
+def flatten_me(lst):
+    arr = []
+    for item in lst:
+        if type(item) == list:
+            for value in item:
+                arr.append(value)
+        else:
+            arr.append(item)
+    return arr
