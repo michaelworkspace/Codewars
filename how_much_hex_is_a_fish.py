2eabc7093971edb11d74542d4dbdb2d0aedeacf8
Michael Le <admin@michaelworkspace.com>
2020-03-11 20:53:45 -0500

    how much hex is a fish

---
+++ how_much_hex_is_a_fish.py
@@ -0,0 +1,24 @@
+"""
+How much is the fish! (- Scooter )
+The ocean is full of colorful fishes. We as programmers want to know the hexadecimal value of these fishes.
+
+Task
+Take all hexadecimal valid characters (a,b,c,d,e,f) of the given name and XOR them. Return the result as an integer.
+
+Input
+The input is always a string, which can contain spaces, upper and lower case letters but no digits.
+
+Example
+fisHex("redlionfish") -> e,d,f -> XOR -> 12
+"""
+
+
+def fisHex(name: str) -> int:
+    hexachars = 'abcdef'
+    name = name.lower()
+    result = 0
+
+    for letter in name:
+        if letter in hexachars:
+            result ^= int(letter, 16)
+    return result
