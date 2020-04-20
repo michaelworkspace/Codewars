844d2600a7cb5538604be39d2e9f5bebe7383dd2
Michael Le <admin@michaelworkspace.com>
2020-04-20 11:59:13 -0500

    sum of minumums

---
+++ sum_of_minimums.py
@@ -0,0 +1,24 @@
+"""
+Given a 2D list of size m * n. Your task is to find the sum of minimum value in each row.
+
+For Example:
+
+[
+  [1, 2, 3, 4, 5],       # minimum value of row is 1
+  [5, 6, 7, 8, 9],       # minimum value of row is 5
+  [20, 21, 34, 56, 100]  # minimum value of row is 20
+]
+So, the function should return 26 because sum of minimums is as 1 + 5 + 20 = 26
+
+Note: You will be always given non-empty list containing Positive values.
+"""
+
+
+from typing import List
+
+
+def sum_of_minimums(numbers: List[int]) -> int:
+    res = 0
+    for item in numbers:
+        res += min(item)
+    return res
