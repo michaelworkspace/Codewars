8bbeb49b44a09a59cca054ecb899fd8e2dd9644a
Michael Le <admin@michaelworkspace.com>
2020-04-23 22:42:51 -0500

    optimizing

--- SimplePigLatin.py
+++ SimplePigLatin.py
@@ -6,6 +6,6 @@ pig_it('Pig latin is cool') # igPay atinlay siay oolcay
 pig_it('Hello world !')     # elloHay orldway !
 """
 
+
 def pig_it(text):
-    
     return ' '.join(x[1:] + x[0] + "ay" if x.isalpha() else x for x in text.split())
