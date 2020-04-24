5c2dee634ba51bcaf0d7ee1b1f9b759994f88e9c
Michael Le <admin@michaelworkspace.com>
2020-04-23 22:23:33 -0500

    refactor

--- SimplePigLatin.py
+++ SimplePigLatin.py
@@ -4,7 +4,6 @@ Move the first letter of each word to the end of it, then add "ay" to the end of
 Examples
 pig_it('Pig latin is cool') # igPay atinlay siay oolcay
 pig_it('Hello world !')     # elloHay orldway !
-
 """
 
 def pig_it(text):
