bfe0634d352e5be59b6704f5fdbeffc541e6e2f2
Michael Le <admin@michaelworkspace.com>
2020-05-26 23:18:15 -0500

    refactor

--- Detect_Pangram.py
+++ Detect_Pangram.py
@@ -4,6 +4,7 @@ A pangram is a sentence that contains every single letter of the alphabet at lea
 Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 """
 
+
 def is_pangram(s):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     pangram = ''
