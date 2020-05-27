3b80513666387e159d6c98742b2ba561e5d1881a
Michael Le <admin@michaelworkspace.com>
2020-05-27 00:10:36 -0500

    refactor

--- two_sum.py
+++ two_sum.py
@@ -1,9 +1,12 @@
 """
-Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple like so: (index1, index2).
+Write a function that takes an array of numbers (integers for the tests) and a target number.
+It should find two different items in the array that, when added together, give the target value.
+The indices of these items should then be returned in a tuple like so: (index1, index2).
 
 For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
 
-The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+The input will always be valid (numbers will be an array of length 2 or greater,
+and all of the items will be numbers; target will always be the sum of two different items from that array).
 
 Based on: http://oj.leetcode.com/problems/two-sum/
 
@@ -12,8 +15,11 @@ twoSum [1, 2, 3] 4 === (0, 2)
 
 
 def two_sum(numbers, target):
-    # naive approach
+    # naive approach: time complexity = O(n)^2, space complexity = O(1)
     for i in range(len(numbers)):
         for j in range(i+1, len(numbers)):
             if numbers[i] + numbers[j] == target:
                 return i, j
+
+print(two_sum([1, 2, 3], 4))
+print(two_sum([4, 2, 8, 15, 0], 17))
